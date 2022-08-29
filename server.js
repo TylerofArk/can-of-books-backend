223170ccd5fb8817966f5aeb3586d35a714d9169
Tyler Main <tyler.l.main@gmail.com>
2022-08-29 17:52:32 -0500

    backend server up and books are in db

--- server.js
+++ server.js
@@ -3,16 +3,43 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const Book = require('./models/book.js');
+
+mongoose.connect(process.env.DB_URL);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected');
+});
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
+app.get('/', (request, response) => {
+  response.status(200).send('Welcome to the server');
+});
 
-app.get('/test', (request, response) => {
+app.get('/books', getBooks);
 
-  response.send('test request received')
+async function getBooks(request, response, next) {
+  try {
+    let results = await Book.find();
 
-})
+    console.log(results);
+    response.status(200).send(results);
+  } catch(error) {
+    next(error);
+  }
+}
+
+// app.get('/Books', (request, response) => {
+
+//   response.send('test request received');
+
+// });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
